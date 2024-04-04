688ebb48014a9c36db8e09939da5a2a0b3a1efb7
Eddie Nieberding <nieberdinge@gmail.com>
2024-04-03 21:39:05 -0400

    added cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { arrayBuffer } from 'stream/consumers';
 
-export default function Home() {    
+export default function Home() {
     
-    return (<div>
-    </div>);
+    let cards = (
+        <div>
+            <div className='calendar'>
+                {createCalendar()}
+            </div>
+        </div>
+    )
+    return cards;
+}
+
+function createCalendar() {
+    let days = []
+    for (let i = 0; i < 31; i++){
+        days.push(<div className='card' key={i}>day {i+1}</div>)
+    }
+    return days
 }
