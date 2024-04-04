a01b344edad38d34ba9114cabe03b9f35871ef86
Eddie Nieberding <nieberdinge@gmail.com>
2024-04-03 20:59:29 -0400

    added bootstraps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-export default function Home() {
-    return     (<div>
+import React from 'react'
 
-    {
-        Array.from({length : 16} , (_, index)  :JSX.Element => {
-            return (
-              <div key={index}>
-               <div className="card">
-                <h1>Click Me!</h1>
-                </div>
-              </div>
-            )
-        })
-      }
+export default function Home() {    
+    
+    return (<div>
     </div>);
 }
