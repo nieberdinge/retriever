9ff80d748e964da0e68bba9a3bd1416b0ff49f13
Eddie Nieberding <nieberdinge@gmail.com>
2024-04-11 20:24:00 -0400

    updated naming

--- src/app/components/Calendar.tsx
+++ src/app/components/Calendar.tsx
@@ -6,18 +6,18 @@ function Calendar() {
 }
 
 function createCalendar() {
-  const now = new Date();
+  const now = new Date("2024-04-08");
   const firstDayOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
   const lastDayOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0);
 
-  const prevMonday = new Date();
-  prevMonday.setDate(firstDayOfMonth.getDate() - ((prevMonday.getDay() + 6) % 7));
+  const firstMonday = new Date();
+  firstMonday.setDate(firstDayOfMonth.getDate() - ((firstMonday.getDay() + 6) % 7));
   const lastSunday = new Date();
   lastSunday.setDate(lastDayOfMonth.getDate() + ((6 - lastSunday.getDay()) % 7));
 
   var daysOfMonth = [];
-  for (prevMonday; prevMonday <= lastSunday; prevMonday.setDate(prevMonday.getDate() + 1)) {
-      daysOfMonth.push(new Date(prevMonday));
+  for (firstMonday; firstMonday <= lastSunday; firstMonday.setDate(firstMonday.getDate() + 1)) {
+      daysOfMonth.push(new Date(firstMonday));
   }
 
   return daysOfMonth.map(day => <Activity key={day.getDay()} day={day}/>)
