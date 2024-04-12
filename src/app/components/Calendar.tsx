0516bcc331185a89084309273ec1fcebecd99919
Eddie Nieberding <nieberdinge@gmail.com>
2024-04-11 21:01:47 -0400

    adding in a navbar

--- src/app/components/Calendar.tsx
+++ src/app/components/Calendar.tsx
@@ -6,23 +6,20 @@ function Calendar() {
 }
 
 function createCalendar() {
-  const now = new Date("2024-04-08");
+  const now = new Date();
   const firstDayOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
   const lastDayOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0);
 
   const firstMonday = new Date();
   firstMonday.setDate(firstDayOfMonth.getDate() - ((firstDayOfMonth.getDay() + 6) % 7));
   const lastSunday = new Date();
-  lastSunday.setDate(lastDayOfMonth.getDate() + ((6 - lastDayOfMonth.getDay() + 1) % 7));
+  lastSunday.setDate(lastDayOfMonth.getDate() + ((7 - lastDayOfMonth.getDay()) % 7));
 
   let daysOfMonth = [];
   for (const curr = new Date(firstMonday); curr <= lastSunday; curr.setDate(curr.getDate() + 1)) {
       daysOfMonth.push(new Date(curr));
   }
 
-  console.debug(daysOfMonth)
-  console.debug(firstDayOfMonth, lastDayOfMonth, firstMonday, lastSunday)
-  console.debug(firstMonday.getDay() % 7)
   return daysOfMonth.map(day => <Activity key={day.getDay()} day={day}/>)
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Calendar from "./components/Calendar";
+import HomeNavbar from "./components/HomeNavbar";
 
 export default function Home() {
   return (
     <div>
+      <HomeNavbar />
       <h3>Activity Calendar</h3>
       <Calendar />
     </div>
