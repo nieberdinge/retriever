3ef9150f857537bfdac421b316a60e29fd802f92
Eddie Nieberding <nieberdinge@gmail.com>
2024-04-11 21:14:08 -0400

    made navbar global

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Calendar from "./components/Calendar";
-import HomeNavbar from "./components/HomeNavbar";
 
 export default function Home() {
   return (
     <div>
-      <HomeNavbar />
       <h3>Activity Calendar</h3>
       <Calendar />
     </div>
